e97d459b025ed53aded955c786d16ea17a78818c
Chris Brown <xoebus@xoeb.us>
2014-11-01 18:20:55 +0000

    use both releases inside the product

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -7,13 +7,11 @@ cd p-concourse
 
 gem install bundler --no-rdoc --no-ri
 
-ls -la ../vsphere-stemcell
-ls -la ../final-release
-
 bundle install
 bundle exec hangar \
   --product-name p-concourse \
   --product-version 0.1 \
   --stemcell-dir ../vsphere-stemcell \
   --release-dir ../final-release \
+  --release-dir ../garden-linux-release \
   --metadata-template metadata/metadata.yml.erb
