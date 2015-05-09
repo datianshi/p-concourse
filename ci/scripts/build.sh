25ad2c4895fb965065ac31c575dbf9bf427614cb
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-05-09 12:28:30 -0700

    Make 1.5.0.0 compatiable product
    - Use new logo.
    - Allow operator to configure resource config
     - workers need more ephemeral disk
     - no need to prevent user from updating resources
     - use t2.small for Discovery
     - use m3.large for Web and PostgreSQL
     - compilation should use c4.xlarge
    - Add atc username and password
    - Update to concourse v0.56 and garden 0.205
    - Use stemcell 2989
    - Update hangar (no need to provide our own stemcell)
    - Set rank to 1
    
    [#98469592]
    
    Signed-off-by: Jeanie Jung <ejung@pivotal.io>

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -15,7 +15,6 @@ echo "Building product..."
 bundle exec hangar \
   --product-name p-concourse \
   --product-version $(cat ../version/number) \
-  --stemcell-dir ../vsphere-stemcell \
   --release-dir ../final-release \
   --release-dir ../garden-linux-release \
   --metadata-template metadata/metadata.yml.erb
